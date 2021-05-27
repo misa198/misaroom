f892272b45b440c26d08085328f1f6ea800c2ff8
Thanh Vu <51451216+misa198@users.noreply.github.com>
2021-05-27 21:50:54 +0700

    adapter for socket io v2 server connect to v3 client

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,9 +6,11 @@ if (process.env.ENV !== 'production') {
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SocketIoAdapter } from './socket-io.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useWebSocketAdapter(new SocketIoAdapter(app, true));
   // app.setGlobalPrefix('api');
   await app.listen(process.env.APP_PORT);
 }
