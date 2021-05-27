1f6717252a0da118fa4a5960251d14262d9a9f3f
Thanh Vu <51451216+misa198@users.noreply.github.com>
2021-05-27 10:20:25 +0700

    update space background

--- client/src/components/SpaceBackground/index.tsx
+++ client/src/components/SpaceBackground/index.tsx
@@ -1,12 +1,23 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import useResizeObserver from "use-resize-observer";
 
 import { Sky, Star } from "./styled";
 
 const SpaceBackground: FC = () => {
+  const { ref, width = 0 } = useResizeObserver<HTMLDivElement>();
+
+  const [starAmount, setStarAmount] = useState<number>(0);
+
+  useEffect(() => {
+    let about = Math.round(width / 19);
+    about = about < 100 ? about : 100;
+    setStarAmount(about);
+  }, [width]);
+
   return (
-    <Sky>
-      {Array.from(Array(100).keys()).map((_, index) => (
-        <Star key={index.toFixed()} />
+    <Sky ref={ref}>
+      {Array.from(Array(starAmount).keys()).map((_, index) => (
+        <Star key={`star_${index * 1}`} />
       ))}
     </Sky>
   );
