f51a57c0b7f683f757238323400ad16182094ab9
Thanh Vu <51451216+misa198@users.noreply.github.com>
2021-05-28 17:38:00 +0700

    item details

--- client/src/containers/Room/components/CallLayoutItem/index.tsx
+++ client/src/containers/Room/components/CallLayoutItem/index.tsx
@@ -1,8 +1,38 @@
 import { FC } from "react";
-import { CallLayoutItemWrapper } from "./styled";
+import useResize from "use-resize-observer";
+import { Mic } from "react-feather";
+
+import {
+  CallLayoutItemWrapper,
+  CallLayoutItemDetails,
+  CallLayoutItemAvatarVoiceDetect,
+  CallLayoutItemAvatar,
+  CallLayoutItemAvatarImg,
+  CallLayoutItemNameWrapper,
+  CallLayoutItemName,
+  CallLayoutItemNameMic,
+} from "./styled";
 
 const CallLayoutItem: FC = () => {
-  return <CallLayoutItemWrapper />;
+  const { ref, width = 0, height = 0 } = useResize();
+
+  return (
+    <CallLayoutItemWrapper ref={ref}>
+      <CallLayoutItemDetails>
+        <CallLayoutItemAvatarVoiceDetect>
+          <CallLayoutItemAvatar width={width} height={height}>
+            <CallLayoutItemAvatarImg />
+          </CallLayoutItemAvatar>
+        </CallLayoutItemAvatarVoiceDetect>
+      </CallLayoutItemDetails>
+      <CallLayoutItemNameWrapper>
+        <CallLayoutItemName>Misa198</CallLayoutItemName>
+        <CallLayoutItemNameMic>
+          <Mic size={14} />
+        </CallLayoutItemNameMic>
+      </CallLayoutItemNameWrapper>
+    </CallLayoutItemWrapper>
+  );
 };
 
 export default CallLayoutItem;
--- client/src/containers/Room/components/CallLayoutItem/styled.ts
+++ client/src/containers/Room/components/CallLayoutItem/styled.ts
@@ -5,4 +5,78 @@ export const CallLayoutItemWrapper = styled.div`
   height: 100%;
   border-radius: 0.25rem;
   background-color: #0d1117;
+  position: relative;
+  user-select: none;
+`;
+
+export const CallLayoutItemDetails = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CallLayoutItemAvatarVoiceDetect = styled.div`
+  padding: 5px;
+  /* background-image: linear-gradient(to top, #0ba360 0%, #3cba92 100%); */
+  border-radius: 50%;
+`;
+
+export const CallLayoutItemAvatarImg = styled.div`
+  width: 90%;
+  height: 90%;
+  background-image: url("https://res.cloudinary.com/dumfvnj9f/image/upload/v1622008718/animals/kraken_ckm2d1.png");
+  background-size: cover;
+`;
+
+export const CallLayoutItemAvatar = styled.div<{
+  height: number;
+  width: number;
+}>`
+  ${(props) =>
+    props.height >= props.width
+      ? `
+    width: ${props.width * 0.3}px;
+    height: ${props.width * 0.3}px;
+    `
+      : `
+    width: ${props.height * 0.3}px;
+    height: ${props.height * 0.3}px;
+    `}
+
+  border-radius: 50%;
+  overflow: hidden;
+  background-image: linear-gradient(
+    -225deg,
+    #3d4e81 0%,
+    #5753c9 48%,
+    #6e7ff3 100%
+  );
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CallLayoutItemNameWrapper = styled.div`
+  position: absolute;
+  max-width: 35%;
+  bottom: 5px;
+  left: 5px;
+  font-weight: 300;
+  display: flex;
+  background-color: rgba(255, 255, 255, 0.2);
+  padding: 2px 5px;
+  border-radius: 0.25rem;
+  overflow: hidden;
+`;
+
+export const CallLayoutItemName = styled.div`
+  font-weight: 300;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+export const CallLayoutItemNameMic = styled.div`
+  margin-left: 8px;
 `;
