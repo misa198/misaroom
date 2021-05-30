636639c53943a23dbc47c441949755401b3fade5
Thanh Vu <51451216+misa198@users.noreply.github.com>
2021-05-30 17:44:35 +0700

    add formik to message input

--- client/src/containers/Room/components/ChatInput/index.tsx
+++ client/src/containers/Room/components/ChatInput/index.tsx
@@ -1,4 +1,6 @@
-import { FC, useState } from "react";
+import { FC, useRef, useState } from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
 import { Play, Image } from "react-feather";
 
 import {
@@ -9,23 +11,43 @@ import {
   ChatInputButton,
 } from "./styled";
 
+const schema = yup.object().shape({
+  content: yup.string().required(),
+});
+
 const ChatInput: FC = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
   const [hideImageButton, setHideImageButton] = useState(false);
 
   function switchImageButton(): void {
     setHideImageButton(!hideImageButton);
   }
 
+  const formik = useFormik({
+    initialValues: { content: "" },
+    validationSchema: schema,
+    onSubmit: (formValues) => {
+      console.log(formValues);
+      if (inputRef.current) inputRef.current.value = "";
+    },
+  });
+
   return (
     <ChatInputWrapper>
       <ChatInputImageButton hide={hideImageButton}>
         <Image size={18} />
       </ChatInputImageButton>
-      <ChatInputForm>
+      <ChatInputForm onSubmit={formik.handleSubmit}>
         <ChatInputTextField
-          placeholder="text a message..."
           onFocus={switchImageButton}
           onBlur={switchImageButton}
+          type="text"
+          placeholder="text a message..."
+          onChange={formik.handleChange}
+          value={formik.values.content}
+          id="content"
+          name="content"
+          ref={inputRef}
         />
         <ChatInputButton>
           <Play size={18} />
