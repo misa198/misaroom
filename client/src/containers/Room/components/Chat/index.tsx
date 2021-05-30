cf9ea0bab4a507e756f9daffff4427e876dcc7c0
Thanh Vu <51451216+misa198@users.noreply.github.com>
2021-05-30 22:36:07 +0700

    sound

--- client/src/containers/Room/components/Chat/index.tsx
+++ client/src/containers/Room/components/Chat/index.tsx
@@ -1,5 +1,6 @@
 import { FC, memo, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import useSound from "use-sound";
 
 import { RootState } from "../../../../store";
 
@@ -14,22 +15,27 @@ import {
   insertMessage,
   increaseNotification,
 } from "../../../../store/slice/room.slice";
+
 import { socket } from "../../../../shared/socket/SocketProvider";
+
+import messageNotificationSound from "../../../../assets/audios/message-notification.mp3";
 
 const Chat: FC = () => {
   const dispatch = useDispatch();
   const showChat = useSelector(
     (state: RootState) => state.room.status.showChat
   );
+  const [play] = useSound(messageNotificationSound);
 
   useEffect((): any => {
     socket.on("new-message", (message) => {
+      if (!showChat) play();
       dispatch(insertMessage(message));
       dispatch(increaseNotification());
     });
 
     return () => socket.off("new-message");
-  }, [dispatch]);
+  }, [dispatch, play, showChat]);
 
   return (
     <ChatWrapper showChat={showChat}>
